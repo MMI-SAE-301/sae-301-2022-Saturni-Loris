45f01377ec09d3e317ae90afce6a36cf1a7d700e
Saturni-Loris <loris.saturni@edu.univ-fcomte.fr>
2022-10-22 12:31:36 +0200

    button personnaliser sur la page d'accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
   <h1 class="text-2xl">Exemple de ...</h1>
+
+  <div>
+    <MontreProfil class="" semelle="blue" />
+
+    <button class=" px-4 bg-blue-900 rounded-md text-white font-semibold hover:bg-sky-700">
+      <RouterLink to="/form">Personnaliser</RouterLink>
+    </button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import MontreProfil from "../components/MontreProfil.vue";
+</script>
