5034c4ebfa0153329e35cbd3d856a8f5f1be7344
Saturni-Loris <loris.saturni@edu.univ-fcomte.fr>
2022-10-26 10:59:59 +0200

    page produit

--- src/pages/produits.vue
+++ src/pages/produits.vue
@@ -2,4 +2,39 @@
     <div class="px-48 pt-36 font-light underline text-blanctext">
         <h1>NOS PRODUITS</h1>
     </div>
+
+    <div class="flex justify-center pt-24 content-between">
+        <div class="">
+            <CuirIntense />
+        </div>
+        <div class="px-32">
+            <BleuClaire />
+        </div>
+        <div class="">
+            <PierreAnth />
+        </div>
+    </div>
+
+    <div class="flex justify-center pt-24 content-between">
+        <div class="">
+            <MauveFleur />
+        </div>
+        <div class="px-32">
+            <BleuMarines />
+        </div>
+        <div class="">
+            <PierreAnth />
+        </div>
+    </div>
 </template>
+
+
+<script setup lang="ts">
+import PierreAnth from "../components/montre/PierreAnth.vue";
+import CuirIntense from "../components/montre/CuirIntense.vue";
+import BleuClaire from "../components/montre/BleuClaire.vue";
+
+
+import MauveFleur from "../components/montre/MauveFleur.vue";
+import BleuMarine from "../components/montre/BleuMarine.vue";
+</script>
