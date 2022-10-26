e585d2c5c27b8abd891b56ea95cb42fc1e7cd6a0
Saturni-Loris <loris.saturni@edu.univ-fcomte.fr>
2022-10-26 09:21:55 +0200

    footer mis en place

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -119,33 +119,6 @@
     </div>
   </div>
 
-
-  <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
-    <div class="sm:flex sm:items-center sm:justify-between">
-      <a href="/" class="flex items-center px-6 mb-4 sm:mb-0">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TikTak</span>
-      </a>
-      <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-        <li>
-          <a href="/" class="mr-4 hover:underline md:mr-6 text-noirfond">ACCUEIL</a>
-        </li>
-        <li>
-          <a href="contact" class="mr-4 hover:underline md:mr-6 text-noirfond">CONTACT</a>
-        </li>
-        <li>
-          <a href="form" class="mr-4 hover:underline md:mr-6 text-noirfond">PERSONNALISER</a>
-        </li>
-        <li>
-          <a href="panier" class="mr-4 hover:underline md:mr-6 text-noirfond">PANIER</a>
-        </li>
-      </ul>
-    </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2022 <a href="/"
-        class="hover:underline">TikTak</a>. Loris Saturni
-    </span>
-  </footer>
-
 </template>
 
 <script setup lang="ts">
